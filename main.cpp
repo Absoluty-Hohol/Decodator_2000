ff957e5857be782989b2d6a54239232d01fd5505
Absoluty-Hohol <alibabanegr2659@gmail.com>
2025-06-03 22:44:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-#include <map>
+#include <string>
+#include "Encoder.h"
+
+using namespace std;
 
 int main()
 {
-  setlocale(LC_ALL,"Russian");
+	setlocale(LC_ALL, "Russian");
 
-  return 0;
+	string input;
+	while (true)
+	{
+		cout << ">";
+		cin >> input;
+		if (input == "exit")
+		{
+			break;
+		}
+		else if (input == "encode")
+		{
+			cout << "Слово для шифра: ";
+			cin.ignore();
+			getline(cin, input);
+			encode(input);
+		}
+	}
+
+	return 0;
 }
