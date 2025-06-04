be908fa6ef274b92eadb1601718ddb34daca8cc1
Absoluty-Hohol <alibabanegr2659@gmail.com>
2025-06-04 18:15:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <functional>
+
+#include "Decoder.h"
 #include "Encoder.h"
 
 using namespace std;
@@ -8,21 +12,45 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	string input;
-	while (true)
-	{
-		cout << ">";
-		cin >> input;
-		if (input == "exit")
-		{
-			break;
-		}
-		else if (input == "encode")
+	map<string, function<void()>> commands;
+	string input,wrong;
+
+	commands["encode"] = [&]()
 		{
 			cout << "Слово для шифра: ";
 			cin.ignore();
 			getline(cin, input);
 			encode(input);
+		};
+	commands["decoder"] = [&]()
+		{
+			cout << "Впишите название файла: ";
+			cin.ignore();
+			getline(cin, input);
+			cout << "Введите слово: ";
+			getline(cin, wrong);
+			decoder(input,wrong);
+		};
+
+	while (true)
+	{
+		cout << ">";
+		cin >> input;
+		
+		if (input == "exit")
+		{
+			break;
+		}
+
+		auto it = commands.find(input);
+		if (it != commands.end())
+		{
+			it->second();
+		}
+
+		else
+		{
+			cout << "Неизвестная команда";
 		}
 	}
 
